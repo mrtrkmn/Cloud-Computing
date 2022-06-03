c468e6098fb7a3730d115cf91c2e361942d8e608
mrtrkmn <mr.turkmen@icloud.com>
2022-06-04 01:22:09 +0200

    update install-tools.sh based on ex2

--- terraform/scripts/install-tools.sh
+++ terraform/scripts/install-tools.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+DOCKER_COMPOSE_VERSION=v2.6.0
+
+BLUE="\033[0;34m"
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+YELLOW="\033[1;33m"
+NC="\033[0m" 
+
+
 INSTALL_MONGODB() {
     
 
@@ -47,7 +56,48 @@ INSTALL_NODEJS() {
     apt install nodejs
 }
 
+
+
+INSTALL_DOCKER_COMPOSE() {
+    sudo curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+}
+
+INSTALL_DOCKER_ENGINE() {
+    printf "${YELLOW}Installing Docker...${NC}\n"
+    curl  --no-progress-meter  -fsSL https://get.docker.com -o get-docker.sh
+    sh get-docker.sh
+    printf "${YELLOW}Adding $USER to docker group...\n"
+    usermod -aG docker $USER
+    printf "${YELLOW} Enabling docker service...\n"
+    systemctl enable docker
+    printf "${YELLOW} Starting docker service...${NC}\n"
+    systemctl start docker
+    rm get-docker.sh
+}
+
+CONFIGURE_DOCKER_ENGINE() {
+    printf "${YELLOW} Stopping docker service...${NC}\n"
+    systemctl stop docker
+    cp /tmp/docker.service /lib/systemd/system/docker.service
+    systemctl daemon-reload
+    systemctl restart docker
+}
+
+RUN_APPLICATION() {
+    docker-compose -f /home/mrturkmen/docker-compose.yml up -d 
+    rm -rf ~/exercise-2/.env
+}
+
 # required for exercise-1 submission
 
 INSTALL_NODEJS
-INSTALL_MONGODB
+#INSTALL_MONGODB # removed since exercise-2 deploys it as docker image
+
+
+# required for exercise-2 submission 
+
+INSTALL_DOCKER_ENGINE
+CONFIGURE_DOCKER_ENGINE
+INSTALL_DOCKER_COMPOSE 
+RUN_APPLICATION
